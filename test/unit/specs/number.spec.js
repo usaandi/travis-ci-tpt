7fa8847509a2d7bfc3baa639bdf9b37e56d40f7d
usaandi <kaurusai@gmail.com>
2018-04-25 09:49:58 +0300

    Wrote 3 test cases

---
+++ test/unit/specs/number.spec.js
@@ -0,0 +1,19 @@
+function double(num) {
+  return num * 2;
+}
+
+// write 2 test cases that are testing success (tobe)
+// and one that is testing not (.not.toBE)
+describe('double', () => {
+  it('write 1 test cases that are testing success (.tobe) 2*2=4', () => {
+    const t1 = double(2);
+    expect(t1).toBe(4);
+  });
+  it('write 2 test cases that are testing success (.tobe) 2*5=10', () => {
+    expect(double(5)).toBe(10);
+  });
+  it('and one that is testing not (.not.toBE)', () => {
+    const t1 = double(5);
+    expect(t1).not.toBe('9');
+  });
+});
